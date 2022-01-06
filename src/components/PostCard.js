65516898c7c234c36b95ac45c27ceb1ccbe16e96
Walker Finlay <walker.finlay@gmail.com>
2022-01-05 20:15:03 -0500

    clickable divs

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 
 import "./PostCard.scss"
 
-const PostCard = (props) => (
-  <div className="post-card-wrapper">
-    <Link to={props.frontmatter.slug}>
-      <div className="post-card">
-        <div className="post-card-top-bar">
-          <h4 className="post-card-title">
-            {props.frontmatter.title}
-          </h4>
-          <h4 className="post-card-date">
-            {props.frontmatter.date}
-          </h4>
-        </div>
-        <p className="post-card-teaser">
-          {props.frontmatter.teaser}
-        </p>
-      </div>
+const CardTitle = (props) => (
+  <h4 className="post-card-title">
+    <Link to={props.slug}>
+      {props.title}
     </Link>
+  </h4>
+);
+
+const PostCard = props => (
+  <div className="post-card"
+    onClick={() => { navigate(props.frontmatter.slug) }}
+  >
+    <div className="post-card-top-bar">
+      <CardTitle
+        slug={props.frontmatter.slug}
+        title={props.frontmatter.title}
+      />
+      <h4 className="post-card-date">
+        {props.frontmatter.date}
+      </h4>
+    </div>
+    <p className="post-card-teaser">
+      {props.frontmatter.teaser}
+    </p>
   </div>
 );
 
