bdcb1de36c729c6fc676644907debc9aa909d64a
Walker Finlay <walker.finlay@gmail.com>
2022-01-23 21:44:54 -0500

    images done

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,15 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-sharp",
+      options: {
+        defaults: {
+          placeholder: "blurred",
+          breakpoints: [750, 1080, 1366, 1920],
+        },
+      },
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
@@ -44,12 +52,6 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts/`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-page-creator",
-      options: {
-        path: `${__dirname}/src/posts`,
       },
     },
     {
---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,43 @@
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React from "react";
+import DelimitedContent from "../../components/DelimitedContent";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+
+const BlogPost = ({
+  data: {
+    mdx: {
+      frontmatter,
+      body,
+      featuredImg,
+    }
+  }
+}) => (
+  <div>
+    <h1>{frontmatter.title}</h1>
+    <h2>{frontmatter.date}</h2>
+    {featuredImg ? <GatsbyImage image={getImage(featuredImg)} /> : <hr />}
+    <DelimitedContent>
+      <MDXRenderer>{body}</MDXRenderer>
+    </DelimitedContent>
+  </div>
+);
+
+export const query = graphql`
+  query($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+      body
+      featuredImg {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
+      }
+    }
+  }
+`;
+
+export default BlogPost;
