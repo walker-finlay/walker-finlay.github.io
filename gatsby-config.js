43c609bd7a1691b351de062eb58a3534b1cd0caf
Walker Finlay <walkerfinlay@Walkers-MacBook-Pro.local>
2022-01-30 00:51:45 -0500

    icons and bug fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/favicon_io/favicon-32x32.png",
       },
     },
     {
